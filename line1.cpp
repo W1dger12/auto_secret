95ad4d8551d564cc7515f30e48dfce75ddbe90cf
Latyshev Ivan Alexandrovich <ivanlatyshev178@gmail.com>
2026-09-22 13:02:47 +0300

    Enterens

---
+++ line1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+     cout << "Welcome to Lost in the pastn" << endl;
+     cout << "You woke up early in the morning" << endl;
+     cout << "You stand up and coming to a table " << endl;
+     cout << "In the table you see an old recorder" << endl;
+     cout << "You turn it on and listen, something is calling you" << endl;
+     cout << "its saying: 'Dont open the third box'" << endl;
+     cout << "In that moment someone rang the door" << endl;
+
+    return 0;
+}
